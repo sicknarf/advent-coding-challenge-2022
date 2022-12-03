7f941ec5a500151feb43f0c30d7d4fb2fea8917e
sicknarf <fgacad@gmail.com>
2022-12-03 14:58:14 -0800

    there were multiple parts to each daygit add . oops

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -15,6 +15,8 @@ for(let i = 0; i < textByLine.length; i++){
 
 let highestIdx = 0
 let highestNum = 0
+let secondHighest = 0
+let thirdHighest = 0
 
 for(let i = 0; i < arrayOfElves.length; i++){
     let sum = 0;
@@ -27,5 +29,33 @@ for(let i = 0; i < arrayOfElves.length; i++){
     }
 }
 
+for(let i = 0; i < arrayOfElves.length; i++){
+    let sum = 0;
+    for(let j = 0; j < arrayOfElves[i].length; j++){
+        sum += parseInt(arrayOfElves[i][j])
+    }
+    if(sum > secondHighest && sum !== highestNum){
+        highestIdx = i
+        secondHighest = sum
+    }
+}
+
+for(let i = 0; i < arrayOfElves.length; i++){
+    let sum = 0;
+    for(let j = 0; j < arrayOfElves[i].length; j++){
+        sum += parseInt(arrayOfElves[i][j])
+    }
+    if(sum > thirdHighest && sum !== highestNum && sum !== secondHighest){
+        highestIdx = i
+        thirdHighest = sum
+    }
+}
+
+
 console.log(`The elf carrying the most calories of berries is the elf at index ${highestIdx} with ${highestNum} calories`)
 
+console.log(`The elf carrying the second most is carrying ${secondHighest} calories`)
+
+console.log(`The elf carrying the third most is carrying ${thirdHighest} calories`)
+
+console.log(`Therefore, altogether, they are carrying ${highestNum+secondHighest+thirdHighest} calories`)
