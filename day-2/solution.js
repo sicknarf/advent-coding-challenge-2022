230f92739f2592e55f3a0f29b21f501b3d707594
sicknarf <fgacad@gmail.com>
2022-12-03 15:42:13 -0800

    completed pt 2 of day 2

--- day-2/solution.js
+++ day-2/solution.js
@@ -3,26 +3,30 @@ const text = fs.readFileSync("./input.txt").toString('utf-8');
 const plays = text.split("\n")
 
 let totalScore = 0
+let properScore = 0
+const rock = 1
+const paper = 2
+const scissors = 3
 
 for(let i = 0; i < plays.length; i++){
     let player = 0
     let opponent = 0
 
     if(plays[i][2] === "X"){
-        totalScore += 1
+        totalScore += rock
         player += 1
     } else if(plays[i][2] === "Y"){
-        totalScore += 2
+        totalScore += paper
         player += 5
     } else if(plays[i][2] === "Z"){
-        totalScore += 3
+        totalScore += scissors
         player += 11
     }
-    if(plays[i][0] === "A"){
+    if(plays[i][0] === "A"){ // rock +1
         opponent -= 1
-    } else if(plays[i][0] === "B"){
+    } else if(plays[i][0] === "B"){ // paper +2
         opponent -= 5
-    } else if(plays[i][0] === "C"){
+    } else if(plays[i][0] === "C"){ // scissors +3
         opponent -= 11
     }
     if(player + opponent === 0){
@@ -33,3 +37,44 @@ for(let i = 0; i < plays.length; i++){
 }
 
 console.log(`if everything goes according to plan on the strategy guide, the total score would be ${totalScore}`)
+
+for (let i = 0; i < plays.length; i++){
+    let playerState = ""
+    if(plays[i][2] === "X"){ // lose
+        playerState = "lose"
+    } else if(plays[i][2] === "Y"){ // draw + 3
+        playerState = "draw"
+        properScore += 3
+    } else if(plays[i][2] === "Z"){ // win + 6
+        playerState = "win"
+        properScore += 6
+    }
+
+    if(plays[i][0] === "A"){
+        if(playerState === "lose"){
+            properScore += scissors
+        } else if(playerState === "draw"){
+            properScore += rock
+        } else if(playerState === "win"){
+            properScore += paper
+        }
+    } else if(plays[i][0] === "B"){
+        if(playerState === "lose"){
+            properScore += rock
+        } else if(playerState === "draw"){
+            properScore += paper
+        } else if(playerState === "win"){
+            properScore += scissors
+        }
+    } else if(plays[i][0] === "C"){
+        if(playerState === "lose"){
+            properScore += paper
+        } else if(playerState === "draw"){
+            properScore += scissors
+        } else if(playerState === "win"){
+            properScore += rock
+        }
+    }
+}
+
+console.log(`However, with the proper interpretation of the strategy guide, your total score SHOULD be ${properScore}`)
