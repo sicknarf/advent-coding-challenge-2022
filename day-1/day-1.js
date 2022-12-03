961b67073816ee5952f37a59d8c4e4a10bab36e0
sicknarf <fgacad@gmail.com>
2022-12-02 21:01:48 -0800

    setup solution for day 2

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,6 +1,31 @@
 const fs = require("fs");
 const text = fs.readFileSync("./input.txt").toString('utf-8');
 const textByLine = text.split("\n")
+const arrayOfElves = []
+let newElf = []
 
-console.log(textByLine)
+for(let i = 0; i < textByLine.length; i++){
+    if(textByLine[i] !== ""){
+        newElf.push(textByLine[i])
+    } else {
+        arrayOfElves.push(newElf)
+        newElf = []
+    }
+}
+
+let highestIdx = 0
+let highestNum = 0
+
+for(let i = 0; i < arrayOfElves.length; i++){
+    let sum = 0;
+    for(let j = 0; j < arrayOfElves[i].length; j++){
+        sum += parseInt(arrayOfElves[i][j])
+    }
+    if(sum > highestNum){
+        highestIdx = i
+        highestNum = sum
+    }
+}
+
+console.log(`The elf carrying the most calories of berries is the elf at index ${highestIdx} with ${highestNum} calories`)
 
---
+++ day-2/solution.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const text = fs.readFileSync("./input.txt").toString('utf-8');
+const textByLine = text.split("\n")
+
+console.log(textByLine[0][0])
