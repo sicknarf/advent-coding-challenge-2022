3deaa2c7f16ae8f201857b2e7fc7c42f4e3ee620
sicknarf <fgacad@gmail.com>
2022-12-06 00:09:04 -0800

    set up the crane base

--- day-5/solution.js
+++ day-5/solution.js
@@ -2,27 +2,26 @@ const fs = require("fs");
 const text = fs.readFileSync("./input.txt").toString('utf-8');
 const input = text.split("\n")
 
-let crateBase = 0
+let baseIdx = 0
 
 for(let i = 0; i < input.length; i++){
     if (parseInt(input[i])===1){
-        crateBase = i
+        baseIdx = i
     }
 }
 
-const baseArray = input[crateBase].split('   ')
+const baseArray = input[baseIdx].split('   ').map(i=>parseInt(i))
+const baseObject = baseArray.reduce((accumulator, value) => {
+    return {...accumulator, [value]: []};
+  }, {})
 
-console.log(baseArray)
 
-// {
-// 1: [array], 
-// 2: [array],
-// 3: [array],
-// 4: [array],
-// 5: [array],
-// 6: [array],
-// 7: [array],
-// 8: [array],
-// 9: [array],
-// }
+for(let i = baseIdx-1; i >= 0; i--){
+    for(let j = 1; j < (4*Object.keys(baseObject).length); j+=4){
+        if(input[i][j] !== ' '){
+            baseObject[parseInt(input[baseIdx][j])].push(input[i][j])
+        }
+    }
+}
 
+console.log(baseObject)
