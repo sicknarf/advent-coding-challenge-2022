c06d247ed7f58bccc68eac3c815fe5cfebec9e39
sicknarf <fgacad@gmail.com>
2022-12-05 20:15:44 -0800

    fixed up input

--- day-4/solution.js
+++ day-4/solution.js
@@ -5,17 +5,17 @@ const sections = input.map(assignment =>
     assignment.split(',')
     )
 
-const assignedSections = []
+console.log(sections)
 
+// parsing the data
 for (let i = 0; i < sections.length; i++){
     for(let j = 0; j < sections[i].length; j++){
-        assignedSections.push(sections[i][j].split('-'))
-        // console.log(sections[i][j].split('-'))
+        sections[i][j] = sections[i][j].split('-')
+        for(let k = 0; k < sections[i][j].length; k++){
+            sections[i][j][k] = parseInt(sections[i][j][k])
+        }
     }
 }
 
-for (let i = 0; i < assignedSections.length; i++){
-    for (let j = 0; j < assignedSections[i].length; j++){
-        assignedSections[i][j] = parseInt(assignedSections[i][j])
-    }
-}
+// starting the solution
+let counter = 0
