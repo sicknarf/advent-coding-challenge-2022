a50ad49f82ba7b457ce34641435e6f5c7be970b2
sicknarf <fgacad@gmail.com>
2022-12-06 00:34:00 -0800

    d5 p1 complete

--- day-5/solution.js
+++ day-5/solution.js
@@ -3,6 +3,7 @@ const text = fs.readFileSync("./input.txt").toString('utf-8');
 const input = text.split("\n")
 
 let baseIdx = 0
+const solutionArray = []
 
 for(let i = 0; i < input.length; i++){
     if (parseInt(input[i])===1){
@@ -24,4 +25,19 @@ for(let i = baseIdx-1; i >= 0; i--){
     }
 }
 
-console.log(baseObject)
+for(let i = baseIdx+2; i < input.length; i++){
+    let instructions = input[i].split(' ')
+    let fromStack = parseInt(instructions[3])
+    let toStack = parseInt(instructions[5])
+    // j is the qty stack
+    for(let j = 0; j < parseInt(instructions[1]); j++){
+        baseObject[toStack].push(baseObject[fromStack].pop())
+    }
+
+}
+
+for(let i = 1; i <= Object.keys(baseObject).length; i++){
+    solutionArray.push(baseObject[i][baseObject[i].length-1])
+}
+
+console.log(`After moving all the boxes around, the top of each stack spelled out ${solutionArray.join('')}`)
