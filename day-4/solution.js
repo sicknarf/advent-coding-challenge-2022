76d41c35af8702a879d56d965b81e6109814f4a4
sicknarf <fgacad@gmail.com>
2022-12-05 21:20:46 -0800

    pausing the work on this

--- day-4/solution.js
+++ day-4/solution.js
@@ -5,8 +5,6 @@ const sections = input.map(assignment =>
     assignment.split(',')
     )
 
-console.log(sections)
-
 // parsing the data
 for (let i = 0; i < sections.length; i++){
     for(let j = 0; j < sections[i].length; j++){
@@ -19,3 +17,26 @@ for (let i = 0; i < sections.length; i++){
 
 // starting the solution
 let counter = 0
+for(let i = 0; i < sections.length; i++){ // index of each pairing
+    let containsFullSet = false
+    const elf1 = sections[i][0]
+    const elf2 = sections[i][1]
+    const firstElfRange = [...Array(elf1[1]-elf1[0]+1).keys()].map(i => i + elf1[0])
+    const secondElfRange = [...Array(elf2[1]-elf2[0]+1).keys()].map(i => i + elf2[0])
+    for(let j=0; j<secondElfRange.length; j++){
+        if(firstElfRange.includes(secondElfRange[j]) || secondElfRange.includes(firstElfRange[j])){
+            containsFullSet = true
+            console.log(`triggered on line ${i+1}`)
+        }
+    }
+    if(containsFullSet){
+        counter++
+        // console.log(`triggered at line ${i+1}`)
+        // console.log(`first elf at ${i}`)
+        // console.log(firstElfRange)
+        // console.log(`second elf at ${i}`)
+        // console.log(secondElfRange)
+    }
+}
+
+console.log(counter)
