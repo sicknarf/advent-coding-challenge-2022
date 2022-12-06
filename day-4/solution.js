eac1cd811e33ec384b0e247c709a3df4e925f03b
sicknarf <fgacad@gmail.com>
2022-12-05 19:23:31 -0800

    converted input from strings to numbers

--- day-4/solution.js
+++ day-4/solution.js
@@ -1,15 +1,21 @@
 const fs = require("fs");
 const text = fs.readFileSync("./input.txt").toString('utf-8');
 const input = text.split("\n")
-
 const sections = input.map(assignment => 
     assignment.split(',')
     )
 
+const assignedSections = []
+
 for (let i = 0; i < sections.length; i++){
     for(let j = 0; j < sections[i].length; j++){
-        sections[i][j].split('-')
+        assignedSections.push(sections[i][j].split('-'))
+        // console.log(sections[i][j].split('-'))
     }
 }
 
-console.log(sections)
+for (let i = 0; i < assignedSections.length; i++){
+    for (let j = 0; j < assignedSections[i].length; j++){
+        assignedSections[i][j] = parseInt(assignedSections[i][j])
+    }
+}
