29316c45f099c95fce9fbff54f3c2d4368a2465d
sicknarf <fgacad@gmail.com>
2022-12-05 22:19:54 -0800

    cleaning up part 1

--- day-4/solution.js
+++ day-4/solution.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const text = fs.readFileSync("./input.txt").toString('utf-8');
-// const text = fs.readFileSync("./input copy.txt").toString('utf-8');
 const input = text.split("\n")
 const sections = input.map(assignment => 
     assignment.split(',')
