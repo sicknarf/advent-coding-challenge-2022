fd17e5e4c7911c1bb59397b42b658d100d97e9ac
sicknarf <fgacad@gmail.com>
2022-12-05 22:29:42 -0800

    completed solution for p2

--- day-4/solution.js
+++ day-4/solution.js
@@ -15,7 +15,7 @@ for (let i = 0; i < sections.length; i++){
     }
 }
 
-// starting the solution
+// starting the solution for pt 1
 let counter = 0
 for(let i = 0; i < sections.length; i++){
     const elf1 = sections[i][0]
@@ -35,3 +35,24 @@ for(let i = 0; i < sections.length; i++){
 }
 
 console.log(`for all the different pairing IDs, there are ${counter} IDs that fully overlap`)
+
+// solution for pt 2
+let counterPt2 = 0
+for(let i = 0; i < sections.length; i++){
+    let containsPart = false
+    const elf1 = sections[i][0]
+    const elf2 = sections[i][1]
+    const firstElfRange = [...Array(elf1[1]-elf1[0]+1).keys()].map(i => i + elf1[0])
+    const secondElfRange = [...Array(elf2[1]-elf2[0]+1).keys()].map(i => i + elf2[0])
+
+    for(let j=0; j<secondElfRange.length; j++){
+        if(firstElfRange.includes(secondElfRange[j]) || secondElfRange.includes(firstElfRange[j])){
+            containsPart = true
+        }
+    }
+    if(containsPart){
+        counterPt2++
+    }
+}
+
+console.log(`For the IDs with any sort of overlap, there are ${counterPt2} total that overlap`)
