27accaaa6781fff4d2c8d27fc437fcd8b0b2d7c8
sicknarf <fgacad@gmail.com>
2022-12-06 21:56:50 -0800

    completed d6 p2

--- day-6/solution.js
+++ day-6/solution.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString('utf-8');
 
+// pt 1
 let solution = 3
 
 for(let i = 3; i < input.length; i++){
@@ -13,3 +14,27 @@ for(let i = 3; i < input.length; i++){
 }
 
 console.log(`After reviewing the signal, the machine processed ${solution} characters before completing.`)
+
+// pt 2
+// dynamically generating arrays for pt 2
+
+let calcArr = 13
+let distinctChar = [...new Array(calcArr+1).keys()]
+
+
+
+for(let i = calcArr; i < input.length; i++){
+    const markerArray = distinctChar.map(num => input[i - num])
+    calcArr++
+    console.log(markerArray)
+    // if(new Set(markerArray).size === 4){
+    //     console.log(markerArray)
+    //     break
+    // }
+    if(new Set(markerArray).size === 14){
+        console.log(markerArray)
+        break
+    }
+}
+
+console.log(`After extending the range from 4 to 14 characters, it turns out that the new signal processes ${calcArr} characters before it finds the start-of-marker packet.`)
