8950c3b4da67111f4652d1c252f64a697a4c6617
sicknarf <fgacad@gmail.com>
2022-12-07 17:01:22 -0800

    d7 p1 setup contd

--- day-7/solution.js
+++ day-7/solution.js
@@ -2,11 +2,33 @@ const fs = require("fs");
 const text = fs.readFileSync("./input.txt").toString('utf-8');
 const input = text.split("\n")
 
-console.log(isNaN(parseInt(input[0])))
-console.log(isNaN(parseInt(input[3])))
-console.log(parseInt(input[3]))
+const fileSystem = {}
 
-function firstWord(n){
-    const myArr = n.split(' ')
-    return myArr[0]
+for(let i = 0; i < input.length; i++){
+    // let ip = input[i].split(' ')
+    // let prev = input[i-1]
+    let cd = 0
+    if (input[i].split(' ')[0] === '$' && input[i].split(' ')[1] === 'ls'){
+        fileSystem[input[i-1].split(' ')[2]] = 0
+        for(let j = i+1; j < input.length; j++){
+            if(parseInt(input[j]) > 0){
+                fileSystem[input[i-1].split(' ')[2]] += parseInt(input[j])
+            }
+            if(input[j].split(' ')[0] === '$'){
+                
+            }
+        }
+    }
+    
+    // if (parseInt(ip[0]) > 0){
+        // parseInt(ip[0]) // this will pull the file on the current line 
+    // }
+}
+
+console.log(fileSystem)
+
+function directoryScan(array){ 
+    for (let i = 0; i < cd.length; i++){
+
+    }
 }
