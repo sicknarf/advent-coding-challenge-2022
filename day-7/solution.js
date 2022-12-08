49169a02603d8b05f3090e5c09e552ef841c3e4f
sicknarf <fgacad@gmail.com>
2022-12-07 19:23:50 -0800

    d7 p1 some progress

--- day-7/solution.js
+++ day-7/solution.js
@@ -5,30 +5,90 @@ const input = text.split("\n")
 const fileSystem = {}
 
 for(let i = 0; i < input.length; i++){
-    // let ip = input[i].split(' ')
-    // let prev = input[i-1]
-    let cd = 0
-    if (input[i].split(' ')[0] === '$' && input[i].split(' ')[1] === 'ls'){
+    // creates an object of all directories, including `/`
+    if (input[i].split(' ')[0] === '$' && input[i].split(' ')[1] === 'ls'){ 
         fileSystem[input[i-1].split(' ')[2]] = 0
-        for(let j = i+1; j < input.length; j++){
-            if(parseInt(input[j]) > 0){
-                fileSystem[input[i-1].split(' ')[2]] += parseInt(input[j])
+    }
+}
+
+for(let i = 1; i < input.length; i++){
+    if (input[i].split(' ')[0] === '$' && input[i].split(' ')[1] === 'ls'){ 
+        // ls(input, i, input[i-1].split(' ')[0])
+    }
+}
+
+// console.log(fileSystem)
+
+// console.log(Object.keys(fileSystem))
+
+// function ls(array, iterator, directoryName){ 
+//     for (let j = iterator; j < array.length; j++){
+//         if(array[j].split(' '[0] === '$')){
+//             break
+//         }
+//         if(array[j].split(' '[0] === 'dir')){
+//             myDirectory = array[j].split(' '[1])
+//         }
+//     }
+//     for (let i = 0; i < array.length; i++){
+//         if(array[i].split(' '[0] === 'cd') && array[i].split(' '[1] === myDirectory)){
+
+//         }
+//     }
+// }
+
+let myArr = ['simulating input']
+let testArr = ['simulating child directory'] 
+let testName = 'simulating directory name'
+
+// child directory: from CD/LS to next $
+
+function count(dir){ // dir is the directory we are looking for
+    for(let i = 0; i < input.length; i++){
+        // go through all of input
+        // look for cd dir
+        // array-ify it until we hit $
+        // call count(dir) again if there is a dir ____ in the array
+        // if no dir - count the numbers
+        // if(input[i].split(' ')[1]==='cd'){
+            //     console.log(input[i].split(' ')[2])
+            // }
+            if(input[i].split(' ')[1] === 'cd' && input[i].split(' ')[2] === dir){
+                let theArr = []
+            for(let j = i+2; j < input.length; j++){
+                if(input[j].split(' ')[0] === '$'){
+                    break
+                }
+                // console.log(input[j])
+                theArr.push(input[j])
             }
-            if(input[j].split(' ')[0] === '$'){
-                
+            for(let j = 0; j < theArr.length; j++){
+                if(parseInt(theArr[j].split(' ')[0]) > 0){
+                    fileSystem[dir] += parseInt(theArr[j].split(' ')[0])
+                    console.log(parseInt(theArr[j].split(' ')[0]))
+                    console.log(theArr[j])
+                } else if (theArr[j].split(' ')[0] === 'dir') {
+                    count(theArr[j].split(' ')[0])
+                } else {
+                    console.log(theArr)
+                }
             }
         }
+        
+        // if(parseInt(theArr.split(' ')[0]) > 0){
+        //     fileSystem[dir] += parseInt(testArr.split(' ')[0])
+        //     fileSystem[testName] += parseInt(testArr.split(' ')[0])
+        // }
+        // if(theArr.split(' ')[0] === 'dir'){
+        //     count(theArr.split(' ')[1])
+        // }
     }
-    
-    // if (parseInt(ip[0]) > 0){
-        // parseInt(ip[0]) // this will pull the file on the current line 
-    // }
 }
 
+// for(let i = 0; i < Object.keys(fileSystem).length; i++){
+//     count(Object.keys(fileSystem)[i])
+// }
+
+Object.keys(fileSystem).forEach(element => count(element))
 console.log(fileSystem)
-
-function directoryScan(array){ 
-    for (let i = 0; i < cd.length; i++){
-
-    }
-}
+console.log(parseInt('dir'))
