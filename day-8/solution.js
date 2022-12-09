7fb98e829b17e3150f041580c3f24e90281f53cc
sicknarf <fgacad@gmail.com>
2022-12-08 23:40:01 -0800

    d8 p1 complete

--- day-8/solution.js
+++ day-8/solution.js
@@ -1,7 +1,39 @@
 const fs = require("fs");
 const text = fs.readFileSync("./input.txt").toString("utf-8");
+// const text = fs.readFileSync("./sampleinput.txt").toString("utf-8");
 const input = text.split("\n");
 
 let forest = input.map(x=> x.split(''))
 
-console.log(forest)
+let visibleTrees = (forest[0].length*2) + ((forest.length-2)*2)
+
+for(let i = 1; i < forest.length-1; i++){
+    for(let j = 1; j < forest[i].length-1; j++){
+        const tree = forest[i][j]
+        const above = []
+        const below = []
+        const left  = []
+        const right = []
+        for(let k = 0; k < i; k++){
+            above.push(forest[k][j])
+        }
+        for(let k = forest.length-1; k > i; k--){
+            below.push(forest[k][j])
+        }
+        for(let k = 0; k < j; k++){
+            left.push(forest[i][k])
+        }
+        for(let k = j+1; k < forest[i].length; k++){
+            right.push(forest[i][k])
+        }
+
+        if( !above.some(x=> x >= parseInt(tree)) || // checking if any of the trees are taller
+            !below.some(x=> x >= parseInt(tree)) ||
+            !left.some(x=> x >= parseInt(tree)) ||
+            !right.some(x=> x >= parseInt(tree))){
+                visibleTrees++
+            }
+    }
+}
+
+console.log(visibleTrees)
