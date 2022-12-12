f418d40e412bac3212fe711b0906abe6295b2865
sicknarf <fgacad@gmail.com>
2022-12-12 11:42:01 -0800

    updated directory names

--- day-10/solution pt 2.js
+++ day-10/solution pt 2.js
@@ -31,37 +31,37 @@ function checkCycle(){
         if(x === position || x-1 === position || x+1 === position){
             solution[0] = solution[0] + '#'    
         }else{
-            solution[0] = solution[0] + '.'
+            solution[0] = solution[0] + ' '
         }
     } else if (cycles > 40 && cycles < 81){
         if(x === position-40 || x-1 === position-40 || x+1 === position-40){
             solution[1] = solution[1] + '#'    
         }else{
-            solution[1] = solution[1] + '.'
+            solution[1] = solution[1] + ' '
         }
     } else if (cycles > 80 && cycles < 121){
         if(x === position-80 || x-1 === position-80 || x+1 === position-80){
             solution[2] = solution[2] + '#'    
         }else{
-            solution[2] = solution[2] + '.'
+            solution[2] = solution[2] + ' '
         }
     } else if (cycles > 120 && cycles < 161){
         if(x === position-120 || x-1 === position-120 || x+1 === position-120){
             solution[3] = solution[3] + '#'    
         }else{
-            solution[3] = solution[3] + '.'
+            solution[3] = solution[3] + ' '
         }
     } else if (cycles > 160 && cycles < 201){
         if(x === position-160 || x-1 === position-160 || x+1 === position-160){
             solution[4] = solution[4] + '#'    
         }else{
-            solution[4] = solution[4] + '.'
+            solution[4] = solution[4] + ' '
         }
     } else if (cycles > 200 && cycles <= 240){
         if(x === position-200 || x-1 === position-200 || x+1 === position-200){
             solution[5] = solution[5] + '#'    
         }else{
-            solution[5] = solution[5] + '.'
+            solution[5] = solution[5] + ' '
         }
     }
 }
